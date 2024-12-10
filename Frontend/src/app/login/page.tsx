8e43bcdcdc23165ce01aac25af70d9bda24ddd4d
aldi2809 <aldanatedesco28@gmail.com>
2024-12-09 22:55:58 -0300

    update login

--- Frontend/src/app/login/page.tsx
+++ Frontend/src/app/login/page.tsx
@@ -35,42 +35,19 @@ export default function Login() {
       if (response.ok) {
         const data = await response.json();
 
-        // Supongamos que el token est en data.token
-        console.log(data)
-        Cookies.set("authToken", data.access, { expires: 7 }); // La cookie expira en 7 das
+        // Supongamos que el token está en data.token
+        console.log(data);
+        Cookies.set("authToken", data.access, { expires: 7 }); // La cookie expira en 7 días
         alert("Login exitoso!");
         console.log("Login exitoso");
         window.location.href = "/";
       } else {
         console.error("Error en el login");
-        alert("Hubo un error al iniciar sesion!");
+        alert("Hubo un error al iniciar sesión!");
       }
     } catch (error) {
-      alert("Hubo un error al iniciar sesion!");
-      console.error("Error en la peticion", error);
-    }
-  };
-
-  const handleGoogleLogin = async () => {
-    try {
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_API_URL}/api/google-auth/`,
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-
-      if (response.ok) {
-        const data = await response.json();
-        window.location.href = data.google_login_url;
-      } else {
-        console.error("Error al obtener la URL de autenticacion de Google");
-      }
-    } catch (error) {
-      console.error("Error en la peticion", error);
+      alert("Hubo un error al iniciar sesión!");
+      console.error("Error en la petición", error);
     }
   };
 
@@ -82,9 +59,7 @@ export default function Login() {
     <div className="min-h-screen bg-gray-100">
       <header className="bg-white shadow-sm">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 flex justify-between items-center">
-          <h1 className="text-2xl font-bold text-gray-900">
-            Smart Fridge Eats
-          </h1>
+          <h1 className="text-2xl font-bold text-gray-900">Smart Fridge Eats</h1>
           <div className="flex items-center space-x-4">
             <Button variant="ghost" onClick={handleSignUpEvent}>
               <LogIn className="mr-2 h-4 w-4" />
@@ -117,39 +92,6 @@ export default function Login() {
             />
             <Button className="w-full" onClick={handleLogin}>
               Login
-            </Button>
-
-            <div className="relative">
-              <div className="absolute inset-0 flex items-center">
-                <span className="w-full border-t" />
-              </div>
-              <div className="relative flex justify-center text-xs uppercase">
-                <span className="bg-white px-2 text-muted-foreground">
-                  Or continue with
-                </span>
-              </div>
-            </div>
-            <Button
-              variant="outline"
-              className="w-full"
-              onClick={handleGoogleLogin}
-            >
-              <svg
-                className="mr-2 h-4 w-4"
-                aria-hidden="true"
-                focusable="false"
-                data-prefix="fab"
-                data-icon="google"
-                role="img"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 488 512"
-              >
-                <path
-                  fill="currentColor"
-                  d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
-                ></path>
-              </svg>
-              Google
             </Button>
           </CardContent>
         </Card>
